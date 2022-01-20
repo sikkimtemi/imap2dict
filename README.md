8029f4be3d8aa9d33e1b533079fb80a1653d09ad
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-01-20 11:19:01 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# imap2dict
+
+## Description
+
+`imap2dict` is a Python library for receiving and deleting emails from an IMAP4 server.
+
+## Installaction
+
+```bash
+pip install imap2dict
+```
+
+## Usage
+
+```python
+from imap2dict import MailClient
+
+host_name = 'mail.example.com'
+user_id = 'foo'
+password = 'password'
+
+cli = MailClient(host_name, user_id, password)
+
+# Receive email (`search_option` and `timezone` are optional.)
+messages = cli.fetch_mail(search_option='UNSEEN', timezone='Asia/Tokyo')
+for msg in messages:
+    print('Subject: {}'.format(msg['subject']))
+    print('Body: {}'.format(msg['body']))
+    print('From: {}'.format(msg['from']))
+    print('To: {}'.format(msg['to']))
+    print('Cc: {}'.format(msg['cc']))
+    print('Date: {}'.format(msg['date']))
+    print('Time: {}'.format(msg['time']))
+    print('Format: {}'.format(msg['format']))
+    print('Message-ID: {}'.format(msg['msg_id']))
+    print('Header: {}'.format(msg['header']))
+    for att in msg['attachments']:
+        print('File Name: {}'.format(att['file_name']))
+        print('File Object: {}'.format(att['file_obj']))
+```
---
+++ setup.py
@@ -0,0 +1,48 @@
+# Author: TAKAHASHI Taro <takahashi.taro@takedasystem.com>
+# Copyright (c) 2022- TAKAHASHI Taro
+# Licence: MIT
+
+from setuptools import setup
+
+DESCRIPTION = 'imap2dict: Receiving and deleting email on an IMAP4 server.'
+NAME = 'imap2dict'
+AUTHOR = 'TAKAHASHI Taro'
+AUTHOR_EMAIL = 'takahashi.taro@takedasystem.com'
+URL = 'https://github.com/sikkimtemi/imap2dict'
+LICENSE = 'MIT'
+DOWNLOAD_URL = URL
+VERSION = '0.1.0'
+PYTHON_REQUIRES = '>=3.6'
+INSTALL_REQUIRES = [
+    'pytz>=2020.1'
+]
+PACKAGES = [
+    'imap2dict'
+]
+KEYWORDS = 'imap imap4 json'
+CLASSIFIERS=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6'
+]
+with open('README.md', 'r', encoding='utf-8') as fp:
+    readme = fp.read()
+LONG_DESCRIPTION = readme
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=URL,
+    packages=PACKAGES,
+    classifiers=CLASSIFIERS,
+    license=LICENSE,
+    keywords=KEYWORDS,
+    install_requires=INSTALL_REQUIRES
+)
